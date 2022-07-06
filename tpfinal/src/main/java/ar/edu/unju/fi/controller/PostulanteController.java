62ee0e1538e0bc94f8d45d4a7c1d3c8198a79c1f
Carlos <Carlos@DESKTOP-PS9OHEA>
2022-07-06 03:10:33 -0300

    Prohibir la postulacion dos veces a la misma Oferta

--- tpfinal/src/main/java/ar/edu/unju/fi/controller/PostulanteController.java
+++ tpfinal/src/main/java/ar/edu/unju/fi/controller/PostulanteController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.entity.Ciudadano;
 import ar.edu.unju.fi.entity.Cv;
+import ar.edu.unju.fi.entity.Postulante;
+import ar.edu.unju.fi.service.ICiudadanoService;
 import ar.edu.unju.fi.service.ICvService;
+import ar.edu.unju.fi.service.IOfertaService;
 import ar.edu.unju.fi.service.IPostuladoService;
 
 @Controller
@@ -22,20 +26,46 @@ public class PostulanteController {
 	IPostuladoService postulanteService;
 	@Autowired
 	ICvService cvService;
+	@Autowired
+	ICiudadanoService ciudadanoService;
+	@Autowired
+	IOfertaService ofertaService;
 	@GetMapping("/postularse/{id}")
 	public ModelAndView postularse(@PathVariable(name="id")Long id,Principal prin)
 	{
-	
-		try {
-		ModelAndView mav = new ModelAndView("postulado_exitoso");
-	    Cv curriculum = cvService.BuscarCvDni( Long.parseLong(prin.getName())).get();
-		postulanteService.guardarPostulante(postulanteService.getPostulante(),prin.getName() , id);
-		
-		return mav;
-		}catch (NoSuchElementException e) {
-			ModelAndView mav = new ModelAndView("error_postular");
-			return mav;
+	 
+		if(cvService.BuscarCvDni( Long.parseLong(prin.getName())).isPresent())
+		{
+			
+			
+		    Ciudadano ciudadano= ciudadanoService.buscarId(Long.parseLong(prin.getName())).get();
+		    Long idCiudadano = ciudadano.getId();
+			if(ofertaService.buscarOferta(id).get().getCiudadanos().contains(ciudadano) )
+			{
+				return new ModelAndView("error_contratado");
+			}else if(postulanteService.buscarPostulante(idCiudadano,id).isPresent())	
+			{
+				System.out.println("Postulante ya postulado");
+				return new ModelAndView("erro_postular_nuevamente");
+			}else
+			{
+				postulanteService.guardarPostulante(postulanteService.getPostulante(),prin.getName() , id);
+				System.out.println("No existe ni contratado ni postulante, guardar");
+				return new ModelAndView("postulado_exitoso");
+			}
+			
+			
+			
+			
+			
+		}else
+		{
+			return new ModelAndView("error_postular");
 		}
+		
+			
+			 
+
 	}
 
 }
